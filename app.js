d745d0495a60baae51260a8d213d00b5bc358173
Anindya Ray <anindyaray44@gmail.com>
2021-04-10 14:02:41 +0530

    added basic login and register

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const app= express();
 const path = require('path');
 const mongoose = require('mongoose');
 const notes = require('./models/notes')
-// Connect MongoDB at default port 27017.
+const session=require('express-session')
+const flash= require('connect-flash')
+const passport = require('passport');
+const LocalStrategy= require('passport-local')
+const User=require('./models/user')
+
 mongoose.connect('mongodb://localhost:27017/Hackathon', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -24,9 +29,62 @@ app.use(express.static("public"))
 
 app.use(express.urlencoded({extended:true}));
 
+const sessionConfig={
+    secret:'somesecret',
+    resave:false,
+    saveUninitialized:true
+}
 
-app.get('/',(req,res)=>{
-    res.sendFile('index.html')
+app.use(session(sessionConfig))
+
+app.use(flash())
+
+app.use((req, res, next)=>{
+    res.locals.error=req.flash('error');
+    res.locals.success=req.flash('success')
+    next();
+})
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+app.get('/',(req, res) =>{
+    res.send('helo')
+}
+)
+
+app.get('/register',(req,res)=>{
+    res.render('register.ejs')
+})
+
+
+app.post('/register',async (req, res)=>{
+    try{
+    const {email,username,password} = req.body;
+    const user=new User({email,username})
+    const registeredUser=await User.register(user,password)
+    console.log(registeredUser)
+    req.flash('success','you are successfully registered')
+    res.redirect('/login')
+    }
+    catch(e){
+        req.flash('error',e.message)
+        res.redirect('/register')
+    }
+})
+
+app.get('/login',(req, res)=>{
+    res.render('login.ejs')
+})
+
+app.post('/login',passport.authenticate('local', { failureFlash:true, failureRedirect:'/login'}),(req, res)=>{
+    req.flash('success','you are successfully logged in')
+        res.redirect('/notes')
 })
 
 app.get('/notes',async (req,res)=>{
@@ -35,6 +93,10 @@ app.get('/notes',async (req,res)=>{
 })
 
 app.get('/notes/new', async (req, res) => {
+    if(!req.isAuthenticated()){
+        req.flash('error','you must be signed in to make notes')
+        return res.redirect('/login')
+    }
     const note= await notes.find();
     res.render('new.ejs',{note})
 })
@@ -44,6 +106,10 @@ app.post('/notes', async (req, res)=>{
         await n.save();
         res.redirect('/notes');
 
+})
+
+app.all('*',(req,res)=>{
+    res.send('sorry amigo')
 })
 
 app.listen('5000',() => {
